ef5d9de55e0455e07ba1398c0e8bbcf3ec38f79c
Sash <33847880+SashaMIT@users.noreply.github.com>
2025-04-04 17:13:30 +0000

    Restored to 'bd943830a6109d62a797449e746f9f7e726680a7'
    Replit-Restored-To: bd943830a6109d62a797449e746f9f7e726680a7

--- client/src/components/VisionPage.tsx
+++ client/src/components/VisionPage.tsx
@@ -502,9 +502,8 @@ export function VisionPage() {
                   initial={{ opacity: 0, y: 20 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
-                  className="rounded-xl p-8 border border-[#444] relative"
+                  className="bg-gradient-to-br from-[#171717] to-[#272727] rounded-xl p-8 border border-[#444] relative"
                 >
-                  {/* Removed orange gradient */}
                   {/* Background elements */}
                   <div className="absolute -top-20 -right-20 w-64 h-64 rounded-full bg-[#F7921A]/10 blur-[80px]"></div>
                   <div className="absolute -bottom-20 -left-20 w-72 h-72 rounded-full bg-[#8BABFF]/10 blur-[100px]"></div>
